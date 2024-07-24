0652ca57faf4589690218382327b3b8ccf16dca0
pokope <152680794+pekopo@users.noreply.github.com>
2024-07-24 12:44:40 +0900

    first commit

---
+++ src/main/java/com/example/SplingBootSample02Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplingBootSample02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplingBootSample02Application.class, args);
+	}
+
+}
